d74c94b93f8b5038aede9afe8d39f1c44f2c8abd
Evan C. Pease <evan.pease@lucidworks.com>
2015-02-23 23:04:39 -0800

    cleanup

--- app/fusion/services/fusion-http.js
+++ app/fusion/services/fusion-http.js
@@ -21,7 +21,6 @@ myModule.factory('fusionHttp', ['$http', function($http) {
 
         getQueryPipeline: function(fusionUrl,pipelineId,collectionId,reqHandlr,params) {
             var url = fusionUrl+"/api/apollo/query-pipelines/"+pipelineId+"/collections/"+collectionId+"/"+reqHandlr;
-            //$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin:password123');
             return $http(
                 {method: 'GET',
                     url: url,
@@ -33,7 +32,6 @@ myModule.factory('fusionHttp', ['$http', function($http) {
         postSignal: function(fusionUrl,collectionId,signalData) {
 
             var url = fusionUrl+'/api/apollo/signals/'+collectionId;
-            //$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin:password123');
             return $http.post(url, signalData);
         },
 
@@ -42,19 +40,15 @@ myModule.factory('fusionHttp', ['$http', function($http) {
             var url = fusionUrl+'/api/apollo/aggregator/jobs/'+collectionId+'_signals/'+jobId;
 
             console.log("Posting to " + url);
-            //$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin:password123');
             return $http.post(url);
         },
 
 
         getSpellCheck: function(fusionUrl,pipelineId,collectionId,q) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin:password123');
             return fusionHttp.getQueryPipeline(fusionUrl,pipelineId,collectionId,"spellcheck",{q:q});
         },
 
         getItemsForItemRecommendations: function(fusionUrl,collectionId,docId,fq) {
-            //http://ec2-54-90-6-131.compute-1.amazonaws.com:8764/api/apollo/recommend/wfm_poc1/itemsForItem?docId=54c0c901bcba6916008b50b0
-            //$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin:password123');
             var url = fusionUrl+"/api/apollo/recommend/"+collectionId+"/itemsForItem" //?docId="+docId+"&fq="+filterString;
             console.log("Getting itemsForItem recommendations from "+ url);
             return $http(
@@ -68,10 +62,8 @@ myModule.factory('fusionHttp', ['$http', function($http) {
         },
 
         getItemsForQueryRecommendations: function(fusionUrl,collectionId,q,fq) {
-            //http://ec2-54-90-6-131.compute-1.amazonaws.com:8764/api/apollo/recommend/wfm_poc1/itemsForItem?docId=54c0c901bcba6916008b50b0
             if (!q) q='*:*';
 
-            //$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin:password123');
             var url = fusionUrl+"/api/apollo/recommend/"+collectionId+"/itemsForQuery"; //?q="+q+"&fq="+filterString;
             console.log('Query for recommendations: ' + q);
             console.log('Filters for recommendations:');
@@ -87,8 +79,6 @@ myModule.factory('fusionHttp', ['$http', function($http) {
             });
         },
         getQueriesForItemRecommendations: function(fusionUrl,collectionId,docId,filterString) {
-            //http://ec2-54-90-6-131.compute-1.amazonaws.com:8764/api/apollo/recommend/wfm_poc1/itemsForItem?docId=54c0c901bcba6916008b50b0
-            //$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin:password123');
             var url = fusionUrl+"/api/apollo/recommend/"+collectionId+"/queriesForItem?docId="+docId+"&fq="+filterString;
             console.log("Getting queriesForItem recommendations from "+ url);
             return $http.get(url);
