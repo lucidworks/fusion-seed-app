c1380c61d4e2d5cbc67371bfa8fed02ec61d50bd
Evan C. Pease <evan.pease@lucidworks.com>
2015-03-20 20:43:37 -0700

    updated proxy server

--- proxy-server/fusion-proxy.js
+++ proxy-server/fusion-proxy.js
@@ -1,20 +1,30 @@
-/**
- * Created by evanpease on 3/20/15.
- */
 var http = require('http');
 
-http.createServer(onRequest).listen(9292);
+var fusionHost = "localhost";
+var fusionPort = 8764;
+var username = "admin";
+var password = "password123";
+
+var server = http.createServer(onRequest).listen(9292);
+
 
 function onRequest(client_req, client_res) {
+
+    client_res.setHeader('Access-Control-Allow-Origin', '*');
+    client_res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    client_res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
     console.log('serve: ' + client_req.url);
 
 
-    var auth = 'Basic ' + new Buffer("admin:password123").toString('base64');
-    var header = {'Authorization': auth};
+    var auth = 'Basic ' + new Buffer(username+":"+password).toString('base64');
+    var header = {
+        'Authorization': auth
+    };
 
     var options = {
-        hostname: 'localhost',
-        port: 8764,
+        hostname: fusionHost,
+        port: fusionPort,
         path: client_req.url,
         method: client_req.method,
         headers: header
@@ -22,7 +32,6 @@ function onRequest(client_req, client_res) {
 
 
     var proxy = http.request(options, function (res) {
-        res.setHeader
         res.pipe(client_res, {
             end: true
         });
